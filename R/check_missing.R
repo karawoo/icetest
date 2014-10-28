8688dadab5a2e3679c5bc66320c34848108c72bb
karawoo <woo.kara@gmail.com>
2014-10-28 13:48:46 -0700

    return list of fields with missing data (#2)

--- R/check_missing.R
+++ R/check_missing.R
@@ -12,9 +12,6 @@ check_missing <- function(dat) {
   cols <- c("year", "season", "researcher", "lakename")
   result <- lapply(dat[, cols], function(x) any(is.na(x)))
   missing <- names(result)[which(sapply(result, isTRUE))]
-  if (length(missing) > 0) {
-    print(paste0("Missing data in field(s): ", paste(missing, collapse = ', ')))
-  } else {
-    print("No missing data in required fields.")
-  }
+  result <- list(missing_data_in_fields = missing)
+  result
 }
