14e9be086e1c913508d64afbd43ab34b76379aa6
karawoo <woo.kara@gmail.com>
2014-10-28 13:52:21 -0700

    test check_missing() function

---
+++ tests/testthat/test_check_missing.R
@@ -0,0 +1,18 @@
+context("Check missing")
+
+check_missing_dat_1 <- data.frame(year = c(1990, 1990), 
+                                  season = c("iceon", "iceoff"), 
+                                  researcher = c("Jane.Doe", "Jane.Doe"), 
+                                  lakename = c("Lake Awesome", "Lake Awesome"), 
+                                  watertemp = c(4.0, 12.5))
+
+check_missing_dat_2 <- data.frame(year = c(1990, 1990), 
+                                  season = c("iceon", "iceoff"), 
+                                  researcher = c(NA, NA), 
+                                  lakename = c("Lake Awesome", NA), 
+                                  watertemp = c(NA, 12.5))
+
+test_that("check_missing identifies missing data from proper columns", {
+  expect_equal(length(check_missing(check_missing_dat_1)[[1]]), 0)
+  expect_true(length(check_missing(check_missing_dat_2)[[1]]) > 0)
+})
