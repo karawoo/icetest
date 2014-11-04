561812789c8a57ae8d47e256f4b27631e6fc8607
karawoo <woo.kara@gmail.com>
2014-11-04 15:58:34 -0800

    change output of check_avemax(). (#7)

--- R/check_avemax.R
+++ R/check_avemax.R
@@ -13,9 +13,17 @@ check_avemax <- function(dat) {
   aves <- grep("^ave", avemax, value = TRUE)
   maxs <- grep("^max", avemax, value = TRUE)
   if (any(!mapply(`>=`, dat[, maxs], dat[, aves]), na.rm = TRUE)) {
-    test <- mapply(`>=`, dat[, maxs], dat[, aves])
-    result <- dat[which(apply(test, 1, function(x) any(!x, na.rm = TRUE))), ]
+    ll <- as.list(as.data.frame(mapply(`>=`, dat[, maxs], dat[, aves])))
+    indices <- lapply(ll, function(x) which(!x))
+    tmplist <- lapply(names(indices), function(x) {
+      dat[indices[[x]], c("year", "season", "lakename", "stationlat", 
+                          "stationlong", x, gsub("max", "ave", x))]
+    })
+    result <- tmplist[lapply(tmplist, nrow) > 0]
+    names(result) <- gsub("max", "", 
+                          names(indices[lapply(indices, length) > 0]))
     result
   }
 }
+
 
--- tests/testthat/test_check_avemax.R
+++ tests/testthat/test_check_avemax.R
@@ -1,14 +1,21 @@
 context("Check averages and maxima")
 
-dat_avemax <- data.frame(year = c(1980, 1980, 1981, 1981),
-                         season = rep(c("iceon", "iceoff"), 2), 
-                         lakename = rep("Lake G", 4), 
-                         avetotphos = c(1, 0.5, 0.75, 10),
-                         maxtotphos = c(2, 0.5, 3, 2.5), 
-                         avezoopmass = c(1000, 700, NA, 500), 
-                         maxzoopmass = c(999, 700, NA, 900), 
+dat_avemax <- data.frame(year = c(1980, 1980, 1981, 1981, 1982, 1982),
+                         season = rep(c("iceon", "iceoff"), 3), 
+                         lakename = rep("Lake G", 6), 
+                         stationlat = rep(53.515694, 6),
+                         stationlong = rep(-137.434421, 6),
+                         avetotphos = c(1, 0.5, 0.75, 10, NA, 3),
+                         maxtotphos = c(2, 0.5, 3, 2.5, NA, 6), 
+                         avezoopmass = c(1000, 700, NA, 500, 999, 1000), 
+                         maxzoopmass = c(999, 700, NA, 900, 900, 1000),
+                         avesuva = rep(1, 6),
+                         maxsuva = rep(2, 6),
                          stringsAsFactors = FALSE)
 
 test_that("check_avemax returns data frame of rows with mixed up ave/max", {
-  expect_true(nrow(check_avemax(dat_avemax)) > 0)
+  expect_true(length(check_avemax(dat_avemax)) > 0)
+  expect_null(check_avemax(dat_avemax[, c("year", "season", "lakename", 
+                                          "stationlat", "stationlong",
+                                          "avesuva", "maxsuva")]))
 })
