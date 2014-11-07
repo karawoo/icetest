06ec18fa66e5c3516390848cfb516f9640ef799d
karawoo <woo.kara@gmail.com>
2014-11-06 17:30:50 -0800

    add numeric fields to sysdata.rda

--- data-raw/datafields.R
+++ data-raw/datafields.R
@@ -1,7 +1,32 @@
 template <- read.csv("./data-raw/IceDataTemplate-Beta2_draft2_24Oct2014.csv", 
                      stringsAsFactors = FALSE)
 
+# all field names
 datafields <- template$fieldname
 
+# fields that should be numeric
+numfields <- c("year", "lakearea", "lakemeandepth", "lakemaxdepth", 
+               "lakeelevation", "watershedarea", "h2oresidence", "lakefetch", 
+               "stationdistance", "stationdepth", "stationlat", "stationlong", 
+               "startday", "startyear", "endday", "endyear", "iceduration", 
+               "periodn", "photicdepth", "sampledepth", "icedepth", "snowdepth",
+               "watertemp", "airtemp", "radiation", "secchidepth", "avetotphos",
+               "maxtotphos", "maxtotphos", "avetotdissphos", "maxtotdissphos",
+               "avetotnitro", "maxtotnitro", "avetotdissnitro",
+               "maxtotdissnitro", "avetotdoc", "maxtotdoc", "avesuva", 
+               "maxsuva", "avecolor", "maxcolor", "avechla", "maxchla",
+               "avephytoppr", "maxphytoppr", "avephytomass", "maxphytomass",
+               "avephytocount", "maxphytocount", "propchloro", "propcrypto",
+               "propcyano", "propdiatom", "propdino", "propotherphyto",
+               "aveciliamass", "maxciliamass", "aveciliacount", "maxciliacount",
+               "avehnfmass", "maxhnfmass", "avehnfcount", "maxhnfcount",
+               "avezoopmass", "maxzoopmass", "avezoopcount", "maxzoopcount",
+               "propdaphnia", "propothercladoc", "propcyclopoid",
+               "propcalanoid", "proprotifer", "propotherzoop", "avebactcount",
+               "maxbactcount", "avebactprod", "maxbactprod", "avebenalgalmass",
+               "maxbenalgalmass", "avebenchla", "maxbenchla", "benamphdens",
+               "bengastrodens", "benbivalvedens", "beninsectdens",
+               "benoligodens")
+
 # saves data to R/sysdata.rda
-use_data(datafields, internal = TRUE)
+use_data(datafields, numfields, internal = TRUE, overwrite = TRUE)
