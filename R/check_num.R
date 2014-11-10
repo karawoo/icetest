6d2c62dc9aad3cda897ac8f68bcecd440bc57802
karawoo <woo.kara@gmail.com>
2014-11-10 11:20:18 -0800

    convert fields to numeric (#8). needs testing.

--- R/check_num.R
+++ R/check_num.R
@@ -20,3 +20,27 @@ check_num <- function(dat) {
 }
 
 
+#' Fix non-numeric fields
+#'
+#' Convert fields from character to numeric.
+#'
+#' @param dat Data frame to be tested.
+#'
+#' @author Kara Woo
+#'
+#' @export
+
+fix_num <- function(dat) {
+  numerics <- sapply(dat[, which(colnames(dat) %in% numfields)], function(x) {
+    ifelse(is.numeric(x) | is.logical(x), TRUE, FALSE)
+  })
+  if(!all(numerics)) {
+    nums <- as.data.frame(lapply(dat[, which(colnames(dat) %in% numfields)],
+                                 function(x) as.numeric(as.character(x))))
+    result <- data.frame(nums, dat[, which(!colnames(dat) %in% numfields)])
+    result <- result[, datafields]
+  } else {
+    result <- dat
+  }
+  result
+}
