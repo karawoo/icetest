52d341bb084351920fb0d3a49d9a3ea059440889
karawoo <woo.kara@gmail.com>
2014-11-10 09:52:37 -0800

    update check_num function

--- R/check_num.R
+++ R/check_num.R
@@ -1,6 +1,7 @@
 #' Check numeric fields
 #' 
-#' Certain fields should be numeric; check that they are.
+#' Certain fields should be numeric; check that they are. Returns a vector of
+#' column names of fields that should be numeric but aren't.
 #' 
 #' @param dat Data frame to be tested.
 #' 
@@ -9,9 +10,13 @@
 #' @export
 
 check_num <- function(dat) {
-  sapply(dat[, which(colnames(dat) %in% numfields)], function(x) {
+  numerics <- sapply(dat[, which(colnames(dat) %in% numfields)], function(x) {
     ifelse(is.numeric(x) | is.logical(x), TRUE, FALSE)
   })
+  if(!all(numerics)) {
+    result <- names(which(!numerics))
+    result
+  }
 }
 
 
