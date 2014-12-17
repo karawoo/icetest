b69fb9a0a3acaaf9b81250b6b670d39483734343
karawoo <woo.kara@gmail.com>
2014-12-17 14:46:02 -0800

    update check_avemax per #18
    need to DRY and write more tests

--- R/check_avemax.R
+++ R/check_avemax.R
@@ -8,46 +8,88 @@
 #' 
 #' @export
 
-check_avemax <- function(dat) {
+check_avemax <- function(dat, flag) {
   avemax <- grep("^(ave|max)", names(dat), value = TRUE)
   maxs <- grep("^max", avemax, value = TRUE)
   aves <- grep("^ave", avemax, value = TRUE)
   ## keep only averages which have a corresponding max
   aves <- aves[which(substr(aves, 4, 1000) %in% substr(maxs, 4, 1000))]
-  ## first check fields with names matching the pattern ave* and max*
-  if (any(!mapply(`>=`, dat[, maxs], dat[, aves]), na.rm = TRUE)) {
-    ## list of each "max" column with booleans for whether it's less than
-    ## average or not
-    ll <- mapply(`>=`, dat[, maxs], dat[, aves], SIMPLIFY = FALSE)
-    ## list of indices of observations that where average > max
-    indices <- lapply(ll, function(x) which(!x))
-    ## list of data frames of these observations
-    tmplist <- lapply(names(indices), function(x) {
-      dat[indices[[x]], c("year", "season", "lakename", "stationlat", 
-                          "stationlong", x, gsub("max", "ave", x))]
-    })
-    ## remove empty data frames
-    result <- tmplist[lapply(tmplist, nrow) > 0]
-    names(result) <- gsub("max", "", 
-                          names(indices[lapply(indices, length) > 0]))
-  }
-  ## also check lake mean/max depth
-  if (any(!mapply(`>=`, dat[, "lakemaxdepth"], 
-                  dat[, "lakemeandepth"]), na.rm = TRUE)) {
-    depth <- list(lakedepth = dat[which(dat$lakemeandepth > dat$lakemaxdepth), 
-                      c("year", "season", "lakename", "stationlat", "stationlong", 
-                        "lakemeandepth", "lakemaxdepth")])
-  }
-  
-  if (exists("result") & exists("depth")) {
-    final <- append(result, depth)
-    final
-  } else if (exists("result")) {
-    final <- result
-    final
-  } else if (exists("depth")) {
-    final <- depth
-    final
+
+  ## check if ave is ever greater than max and/or if one of them is ever missing
+  ## when the other is not
+  if (flag == "values") {
+    ## first check fields with names matching the pattern ave* and max*
+    if (any(!mapply(`>=`, dat[, maxs], dat[, aves]), na.rm = TRUE)) {
+      ## list of each "max" column with booleans for whether it's less than
+      ## average or not
+      ll <- mapply(`>=`, dat[, maxs], dat[, aves], SIMPLIFY = FALSE)
+      ## list of indices of observations that where average > max
+      indices <- lapply(ll, function(x) which(!x))
+      ## list of data frames of these observations
+      tmplist <- lapply(names(indices), function(x) {
+        dat[indices[[x]], c("year", "season", "lakename", "stationlat", 
+                            "stationlong", x, gsub("max", "ave", x))]
+      })
+      ## remove empty data frames
+      result <- tmplist[lapply(tmplist, nrow) > 0]
+      names(result) <- gsub("max", "", 
+                            names(indices[lapply(indices, length) > 0]))
+    }
+    ## also check lake mean/max depth
+    if (any(!mapply(`>=`, dat[, "lakemaxdepth"], 
+                    dat[, "lakemeandepth"]), na.rm = TRUE)) {
+      depth <- list(lakedepth = dat[which(dat$lakemeandepth > dat$lakemaxdepth), 
+                        c("year", "season", "lakename", "stationlat", "stationlong", 
+                          "lakemeandepth", "lakemaxdepth")])
+    }
+    
+    if (exists("result") & exists("depth")) {
+      final <- append(result, depth)
+      final
+    } else if (exists("result")) {
+      final <- result
+      final
+    } else if (exists("depth")) {
+      final <- depth
+      final
+    }
+    ## Check for missing values if either average or maximum is present:
+  } else if (flag == "missing") {
+    if(any(!mapply(function(a, b) mapply(compNA, a, b), dat[, maxs], dat[, aves]))) {
+      ll <- mapply(function(a, b) mapply(compNA, a, b), dat[, maxs], dat[, aves],
+                   SIMPLIFY = FALSE)
+      indices <- lapply(ll, function(x) which(!x))
+      tmplist <- lapply(names(indices), function(x) {
+        dat[indices[[x]], c("year", "season", "lakename", "stationlat", 
+                            "stationlong", x, gsub("max", "ave", x))]
+      })
+      result <- tmplist[lapply(tmplist, nrow) > 0]
+      names(result) <- gsub("max", "",
+                            names(indices[lapply(indices, length) > 0]))
+    }
+    if (exists("result")) {
+      result
+    }
   }
 }
 
+#' Compare ave and max for NAs
+#'
+#' Returns TRUE if both or neither argument is NA; returns FALSE if one is NA but the other is not.
+#'
+#' @param a first argument
+#' @param b second argument
+#'
+#' @author Kara Woo
+
+compNA <- function(a, b) {
+  if (is.na(a) && is.na(b) == TRUE) {
+    TRUE    
+  } else if (!is.na(a) && !is.na(b) == TRUE) {
+    TRUE
+  } else if (is.na(a) && !is.na(b) == TRUE) {
+    FALSE
+  } else if (!is.na(a) && is.na(b) == TRUE) {
+    FALSE
+  }
+}
--- tests/testthat/test_check_avemax.R
+++ tests/testthat/test_check_avemax.R
@@ -1,24 +1,25 @@
 context("Check averages and maxima")
 
-dat_avemax <- data.frame(year = c(1980, 1980, 1981, 1981, 1982, 1982),
-                         season = rep(c("iceon", "iceoff"), 3), 
-                         lakename = rep("Lake G", 6), 
-                         stationlat = rep(53.515694, 6),
-                         stationlong = rep(-137.434421, 6),
-                         lakemaxdepth = c(rep(500, 5), 250),
-                         lakemeandepth = c(rep(250, 5), 500),
-                         avetotphos = c(1, 0.5, 0.75, 10, NA, 3),
-                         maxtotphos = c(2, 0.5, 3, 2.5, NA, 6), 
-                         avezoopmass = c(1000, 700, NA, 500, 999, 1000), 
-                         maxzoopmass = c(999, 700, NA, 900, 900, 1000),
-                         avesuva = rep(1, 6),
-                         maxsuva = rep(2, 6),
+dat_avemax <- data.frame(year = c(1980, 1980, 1981, 1981, 1982, 1982, 1983, 1983),
+                         season = rep(c("iceon", "iceoff"), 4), 
+                         lakename = rep("Lake G", 8), 
+                         stationlat = rep(53.515694, 8),
+                         stationlong = rep(-137.434421, 8),
+                         lakemaxdepth = c(rep(500, 7), 250),
+                         lakemeandepth = c(rep(250, 7), 500),
+                         avetotphos = c(1, 0.5, 0.75, 10, NA, 3, 0.25, NA),
+                         maxtotphos = c(2, 0.5, 3, 2.5, NA, 6, NA, NA), 
+                         avezoopmass = c(1000, 700, NA, 500, 999, 1000, 1000, NA), 
+                         maxzoopmass = c(999, 700, NA, 900, 900, 1000, 1000, 900),
+                         avesuva = rep(1, 8),
+                         maxsuva = rep(2, 8),
                          stringsAsFactors = FALSE)
 
 test_that("check_avemax returns data frame of rows with mixed up ave/max", {
-  expect_true(length(check_avemax(dat_avemax)) > 0)
+  expect_true(length(check_avemax(dat_avemax, flag = "values")) > 0)
   expect_null(check_avemax(dat_avemax[1:4, c("year", "season", "lakename", 
                                              "stationlat", "stationlong",
                                              "lakemaxdepth", "lakemeandepth",
-                                             "avesuva", "maxsuva")]))
+                                             "avesuva", "maxsuva")],
+                           flag = "values"))
 })
