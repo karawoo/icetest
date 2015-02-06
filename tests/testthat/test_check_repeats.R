a848c18e7dc327ce8f208668d6321aca0281aab2
karawoo <woo.kara@gmail.com>
2015-02-05 16:20:30 -0800

    add stationname when checking for repeat obs.

--- tests/testthat/test_check_repeats.R
+++ tests/testthat/test_check_repeats.R
@@ -1,10 +1,11 @@
 context("Check repeated observations")
 
-dat_repeats <- data.frame(year = rep(1975, 4), 
-                          season = rep(c("iceon", "iceoff"), 2), 
-                          lakename = c("Lake E", "Lake F", "Lake F", "Lake F"), 
-                          stationlat = c(48.113155, rep(48.113630, 3)), 
-                          stationlong = c(-138.724822, rep(-138.770224, 3)))
+dat_repeats <- data.frame(year = rep(1975, 6), 
+                          season = rep(c("iceon", "iceoff"), 3), 
+                          lakename = c("Lake E", rep("Lake F", 5)),
+                          stationname = c("1658", rep("9011", 3), rep(NA, 2)),
+                          stationlat = c(48.113155, rep(48.113630, 5)), 
+                          stationlong = c(-138.724822, rep(-138.770224, 5)))
 
 test_that("check_repeats returns duplicate rows if they exist", {
   expect_true(nrow(check_repeats(dat_repeats)) > 0)
