db7d02b1a66ebb88275497977cfe1d729c912828
Kara Woo <woo.kara@gmail.com>
2015-06-30 16:06:46 -0700

    Clarify description of check_repeats

--- R/check_repeats.R
+++ R/check_repeats.R
@@ -1,7 +1,10 @@
 #' Check repeats
 #' 
 #' There shouldn't be more than one observation from a single site in a single
-#' year and season.
+#' year and season. This function checks for repeats in both stationname and
+#' latitude/longitude. Note that if there are multiple stations with different
+#' coordinates and no names (i.e. NA in the stationname field), these will be
+#' shown as repeats.
 #' 
 #' @param dat Data frame to be tested.
 #' 
@@ -19,10 +22,9 @@ check_repeats <- function(dat) {
     dd[duplicated(dd[, c("year", "season", "lakename", "stationname")])
        | duplicated(dd[, c("year", "season", "lakename", "stationname")],
                     fromLast = TRUE)
-     | duplicated(dd[, c("year", "season", "lakename",
-                         "stationlat", "stationlong")])
-     | duplicated(dd[, c("year", "season", "lakename",
-                              "stationlat", "stationlong")], fromLast = TRUE), ]
+       | duplicated(dd[, c("year", "season", "lakename",
+                           "stationlat", "stationlong")])
+       | duplicated(dd[, c("year", "season", "lakename",
+                           "stationlat", "stationlong")], fromLast = TRUE), ]
   }
 }
-
